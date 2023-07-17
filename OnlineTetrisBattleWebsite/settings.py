44da43d2126c730767850a0f7b0239103d3aae20
Nguyễn Thanh Châu <chaunguyenthanh7@outlook.com>
2023-07-17 17:37:49 +0700

    Update settings.py

--- OnlineTetrisBattleWebsite/settings.py
+++ OnlineTetrisBattleWebsite/settings.py
@@ -140,6 +140,6 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 
-CSRF_TRUSTED_ORIGINS = ['https://*']
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app', 'https://*.ptit.edu.vn']
 
 SECRET_KEY = 'PTIT@ADMIN@2023@942851'

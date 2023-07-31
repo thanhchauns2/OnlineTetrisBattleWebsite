b3ad666948471417e814f0380972205e90843fa7
Nguyễn Thanh Châu <chaunguyenthanh7@outlook.com>
2023-07-31 13:06:13 +0700

    dong register

--- registration/urls.py
+++ registration/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
+    # path('register/', views.register, name='register'),
     path('login/', views.__login, name='login'),
     path('logout/', views.logout_view, name='logout'),
 ]
